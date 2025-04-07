3eff661cc5460522d4c312c5b3791499b4bee9f0
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-04-07 10:58:42 +0200

    Update #4 js

--- assests/js/basic.js
+++ assests/js/basic.js
@@ -9,5 +9,9 @@ mySubmitButton.addEventListener("click",(e)=> {
     e.preventDefault(); // Prevents the default action of the button, which is to submit the form
 
     // console.log("You clicked the button!"); // Logs a message to the console when the button is clicked
+    // find værdier i input felterne
+    const myName = document.getElementById("name").value; // Gets the value of the input field with id "name"
+    const myEmail = document.getElementById("email").value; // Gets the value of the input field with id "email"
+    console.log(`Name: ${myName}, Email: ${myEmail}`);
 });
 
